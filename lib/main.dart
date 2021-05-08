e4e30777100f2ab04d8f252772503a26610542e4
unknown <jayeshsadhwani99@gmail.com>
2021-05-08 12:10:57 +0530

    Add Pokemon Detail

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 
 import 'package:pokeapp/pokemon.dart';
 
+import 'pokemon_detail.dart';
+
 void main() => runApp(MaterialApp(
       title: 'Poke App',
       home: HomePage(),
@@ -54,29 +56,42 @@ class HomePageState extends State<HomePage> {
                   .map((poke) => Padding(
                         padding: const EdgeInsets.all(2.0),
                         child: InkWell(
-                          onTap: () {},
-                          child: Card(
-                            elevation: 3.0,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: <Widget>[
-                                Container(
-                                  height: 100.0,
-                                  width: 100.0,
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                      image: NetworkImage(poke.img),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => PokeDetail(
+                                  pokemon: poke,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Hero(
+                            tag: poke.img,
+                            child: Card(
+                              elevation: 3.0,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: <Widget>[
+                                  Container(
+                                    height: 100.0,
+                                    width: 100.0,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        image: NetworkImage(poke.img),
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Text(
-                                  poke.name,
-                                  style: TextStyle(
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                )
-                              ],
+                                  Text(
+                                    poke.name,
+                                    style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
                           ),
                         ),
